5ad0013a53cf8034931dcb54ea9fb8f96ef93f58
HariharanM <hariakash1117@gmail.com>
2023-05-31 15:10:08 +0530

    Add project files.

---
+++ HandOn4/EnumCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandOn4
+{
+    //Creating a Enumeration type
+    enum EnumCheck
+    {
+        Enum1=10,
+        Enum2=20,
+        Enum3=30,
+    }
+    
+}
+
---
+++ HandOn4/Program.cs
@@ -0,0 +1,29 @@
+﻿using HandOn4;
+
+Console.WriteLine("Enumeration");
+EnumCheck enumCheck1 = EnumCheck.Enum1!;
+Console.WriteLine("\n");
+int a = 10;
+Console.WriteLine(a.GetType());
+//Check type of Enum1 which is EnumCheck type
+Console.WriteLine(enumCheck1.GetType());
+//Returning Enum1 from EnumCheck type
+Console.WriteLine(enumCheck1);
+//Returning data on Enum1 from EnumCheck type
+Console.WriteLine((int)enumCheck1);
+
+var b = (EnumCheck)0;
+Console.WriteLine(b);
+
+//Null Safety 
+
+Console.WriteLine("Null Safety\n");
+
+//Assign with ? opertor to make the variable can be null and has value
+int? c=null;
+if(c==null)
+{
+    Console.WriteLine("null");
+}
+c = 10;
+Console.WriteLine(c!); 
